b92315db90c2e0655264a724dc8d1b8717ae6a9b
Stephen Tian <745752067@qq.com>
2018-12-23 23:19:03 +0800

    v-time self-directive

--- 02-Books/01-Vue.js实战/05-v-time/time.js
+++ 02-Books/01-Vue.js实战/05-v-time/time.js
@@ -50,12 +50,25 @@ var Time = {
     } else if (timer < 3600) {
       tip = Math.floor(timer / 60) + '分钟前'
     } else if (timer >= 3600 && (timestamp - today) >= 0) {
-      tip = Math.floor(timmer / 3600) + '小时前'
-    } else if (timmer / 86400 <= 31) {
-      tip = Math.ceil(timmer / 86400) + '天前'
+      tip = Math.floor(timer / 3600) + '小时前'
+    } else if (timer / 86400 <= 31) {
+      tip = Math.ceil(timer / 86400) + '天前'
     } else {
       tip = this.getLastDate(timestamp)
     }
     return tip
   }
 }
+
+Vue.directive('time', {
+  bind: function (el, binding) {
+    el.innerHTML = Time.getFormatTime(binding.value)
+    el.__timeout__ = setInterval(function () {
+      el.innerHTML = Time.getFormatTime(binding.value)
+    }, 6000)
+  },
+  unbind: function (el) {
+    clearInterval(el.__timeout__)
+    delete el.__timeout__
+  }
+})
