bbe4d15bd2ed0871b9be77a97dd81c26ebf3da7b
Stephen Tian <745752067@qq.com>
2019-01-02 20:56:44 +0800

    sortable methods

--- 02-Books/01-Vue.js实战/10-Sortable Table/table.js
+++ 02-Books/01-Vue.js实战/10-Sortable Table/table.js
@@ -15,8 +15,29 @@ Vue.component('vTable', {
   },
   data: function () {
     return {
-      currentColimns: [],
+      currentColumns: [],
       currentData: []
     }
+  },
+  methods: {
+    makeColumns: function () {
+      this.currentColumns = this.columns.map(function (col, index) {
+        // 添加一个字段标识当前列排序的状态，后续使用
+        col.sortType = 'normal'
+        // 添加一个字符标志当前列在数组中的索引，后续使用
+        col.index = index
+        return col
+      })
+    },
+    makeData: function () {
+      this.currentData = this.data.map(function (row, index) {
+        row.index = index
+        return row
+      })
+    }
+  },
+  mounted() {
+    this.makeColumns()
+    this.makeData()
   }
 })
