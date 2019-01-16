263b6c1d2591fe94b3e84c8ca93b8772b3f634bd
Stephen Tian <745752067@qq.com>
2019-01-16 21:33:45 +0800

    goodsinfo Components

--- 03-Demo/e-commerce/src/views/home/components/GoodsInfo.vue
+++ 03-Demo/e-commerce/src/views/home/components/GoodsInfo.vue
@@ -1,16 +1,44 @@
 <template>
-  <div>
-    <h4>goods</h4>
+  <div class="goods-info"
+       @click="goGoodsPage()">
+    <div class="goods-image">
+      <img v-lazy="goodsImage"
+           width="90%" />
+    </div>
+    <div class="goods-name">{{ goodsName }}</div>
+    <div class="goods-price">￥{{ goodsPrice | moneyFilter }}</div>
   </div>
 </template>
 
 <script>
 export default {
+  props: [
+    'goodsImage',
+    'goodsName',
+    'goodsPrice',
+    'goodsId'
+  ],
   data () {
     return {}
+  },
+  filters: {
+    moneyFilter (m) {
+      return m.toFixed(2)
+    }
+  },
+  metheds: {
+    goGoodsPage () {
+      this.$router.push({ name: 'Goods', query: { goodsId: this.goodsId } })
+    }
   }
 }
 </script>
 
 <style scoped>
+.goods-name {
+  padding: 0 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
 </style>
