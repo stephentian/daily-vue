f03d3b06753ee98678ae9275e324ee8c68ddef04
stephentian <745752067@qq.com>
2019-01-18 22:51:55 +0800

    koa listen

--- 03-Demo/e-commerce/server/index.js
+++ 03-Demo/e-commerce/server/index.js
@@ -1,2 +1,37 @@
 const Koa = require('koa')
 const app = new Koa()
+const { connect, initSchemas } = require('database/init.js')
+const mongoose = require('mongoose')
+const bodyParser = require('koa-bodyparser')
+const cors = require('koa2-cors')
+const Router = require('koa-router')
+
+app.use(bodyParser())
+app.use(cors())
+
+let user = require('./appApi/user.js')
+let home = require('appApi/home.js')
+let goods = require('appApi/goods.js')
+
+let router = new Router()
+
+router.use('/use', user.routes())
+router.use('home', home.routes())
+router.use('goods', goods.routes())
+
+app.use(router.routes())
+app.use(router.allowedMethods)
+
+;(async () => {
+  await connect()
+  initSchemas
+})()
+
+app.use(async (ctx) => {
+  ctx.body = '<h1>Hello Service</h1>'
+})
+
+app.listen(3000, () => {
+  console.log('Server is starting at port 3000...');
+  
+})
