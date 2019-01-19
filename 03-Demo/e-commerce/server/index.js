cb139143583d708698ca80310cd8b2919afc882d
Stephen Tian <745752067@qq.com>
2019-01-19 22:02:44 +0800

    db

--- 03-Demo/e-commerce/server/index.js
+++ 03-Demo/e-commerce/server/index.js
@@ -1,28 +1,32 @@
 const Koa = require('koa')
 const app = new Koa()
-const { connect, initSchemas } = require('database/init.js')
+const {
+  connect,
+  initSchemas
+} = require('./database/init.js')
 const mongoose = require('mongoose')
-const bodyParser = require('koa-bodyparser')
-const cors = require('koa2-cors')
-const Router = require('koa-router')
+// const bodyParser = require('koa-bodyparser')
+// const cors = require('koa2-cors')
+// const Router = require('koa-router')
 
-app.use(bodyParser())
-app.use(cors())
+// app.use(bodyParser())
+// app.use(cors())
 
-let user = require('./appApi/user.js')
-let home = require('appApi/home.js')
-let goods = require('appApi/goods.js')
+// let user = require('./appApi/user.js')
+// let home = require('appApi/home.js')
+// let goods = require('appApi/goods.js')
 
-let router = new Router()
+// let router = new Router()
 
-router.use('/use', user.routes())
-router.use('home', home.routes())
-router.use('goods', goods.routes())
+// router.use('/use', user.routes())
+// router.use('home', home.routes())
+// router.use('goods', goods.routes())
 
-app.use(router.routes())
-app.use(router.allowedMethods)
+// app.use(router.routes())
+// app.use(router.allowedMethods)
 
-;(async () => {
+;
+(async () => {
   await connect()
   initSchemas
 })()
@@ -32,6 +36,5 @@ app.use(async (ctx) => {
 })
 
 app.listen(3000, () => {
-  console.log('Server is starting at port 3000...');
-  
+  console.log('Server is starting at port 3000...')
 })
