977d5080c5173220ca826f290471541137af6142
Stephen Tian <745752067@qq.com>
2019-01-20 21:49:20 +0800

    db connectTimes

--- 03-Demo/e-commerce/server/database/init.js
+++ 03-Demo/e-commerce/server/database/init.js
@@ -5,22 +5,44 @@
  **/
 
 const mongoose = require('mongoose')
-
 const db = 'mongodb://localhost/regou-bd'
+const glob = require('glob')
+const {
+  resolve
+} = require('path')
 
-mongoose.Promise = global.Promise
+exports.initSchemas = () => {
+  glob.sync(resolve(__dirname, './schema', '**/*.js').forEach(require))
+}
+
 
 exports.connect = () => {
   mongoose.connect(db)
-  mongoose.connection.on('disconnected', () => {
-    console.log('Connect to database fail!')
-    mongoose.connect(db)
-  })
-  mongoose.connection.on('error', err => {
-    console.log('Error:', err)
-    mongoose.connect(db)
-  })
-  mongoose.connection.once('open', () => {
-    console.log('Connect to database successfully!')
+  let connectTimes = 0
+  return new Promise((resolve, reject) => {
+    mongoose.connection.on('disconnected', () => {
+      console.log('Connect to database fail!')
+      if (connectTimes <= 3) {
+        connectTimes++
+        mongoose.connect(db)
+      } else {
+        reject()
+        throw new Error('Datebase is Error!')
+      }
+    })
+    mongoose.connection.on('error', err => {
+
+      if (connectTimes <= 3) {
+        connectTimes++
+        mongoose.connect(db)
+      } else {
+        reject(err)
+        throw new Error('Datebase is Error!')
+      }
+    })
+    mongoose.connection.once('open', () => {
+      console.log('Connect to database successfully!')
+      resolve()
+    })
   })
 }
